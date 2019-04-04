eadc9e15255a5e153245a8f58aad09a1bab6faec
Kalou37 <kalnet17@gmail.com>
2019-04-04 13:08:01 +0200

    Initial commit

---
+++ Enemy.gd
@@ -0,0 +1,119 @@
+extends Node2D
+
+#an ID for the enemy
+export var ID : String
+
+#its start_position on the grid
+export var start_position : Vector2
+
+#var UP = Vector2(0,-1)
+#var DOWN = Vector2(0,1)
+#var LEFT = Vector2(-1,0)
+#var RIGHT = Vector2(1,0)
+#var IDLE = Vector2(0,0)
+
+#pointer to the AnimatedSprite cild
+onready var anim = $AnimatedSprite
+
+#pointer to the grid
+onready var grid = get_parent()
+
+#pointer to the Tween child
+#in game development tweening is the process of
+#calcultating interpolations between two positions
+#to move an object
+onready var tween = $AnimatedSprite/Tween
+
+#the diferent behaviors of the enemy
+#allows us to implement a state machine for the enemy
+#	"if I am in this state, I know I must do precisely this thing
+#-> FOLLOW : Follow pacman and try to eat him
+#-> FLEE : Pacman can eat ghots we must flee him
+#-> DEAD : I'm dead, I go back to my starting point
+#-> VICTORIOUS : one of us has eaten Pacman but we still need to go somewhere
+enum behavior {FOLLOW, FLEE, DEAD, VICTORIOUS}
+
+#the starting behavior
+var current_behavior = behavior.FOLLOW
+
+#we always know where the player is (updated each frame)
+var player_position : Vector2
+
+#The position of the enemy in grid coordinates
+var grid_position : Vector2
+
+#Set the enemy position to its init posisition
+func _ready():
+	pass
+	#position = grid.map_to_world(start_position)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var objective = Vector2()
+	var move_time : float
+	var next_position = Vector2()
+	grid_position = grid.get_enemy_pos(ID)
+	#The Enemy must act according to its behavior
+	if current_behavior == behavior.FOLLOW:
+		#I'm following, my objective is Pacman
+		objective = grid.get_player_pos()
+		anim.play("follow")
+		move_time = 0.3
+		next_position = grid.astar_get_next_cell(grid_position,objective)
+	elif current_behavior == behavior.FLEE:
+		#I'm fleeing, mmy objective is far away from pacman
+		objective = grid.map_size
+		anim.play("flee")
+		move_time = 0.3
+		next_position = grid.astar_get_next_cell(grid_position,objective)
+	elif current_behavior == behavior.DEAD:
+		#I'm dead I blink and go back home
+		next_position = start_position
+		anim.play("blink")
+		move_time = 1.5
+		if grid_position == start_position :
+			current_behavior = behavior.FOLLOW
+	elif current_behavior == behavior.VICTORIOUS:
+		#I go back home to bring my trophy
+		next_position = start_position
+		anim.play("follow")
+		move_time = 1.5
+		if grid_position == start_position :
+			current_behavior = behavior.FOLLOW
+	
+	print(current_behavior)
+	#I get my next position from the grid
+
+	if next_position == Vector2():
+		return
+	#when process is set to false the _process function is not called until
+	#until it is set back to true, this allows us to perform e full animation
+	#and motion of the enemy before trying to get it's next position
+	#without this, the enemy would almost teleport itself on its target
+	set_process(false)
+	#this allows us to move smoothly from on cell to the next
+	tween.interpolate_property(self,"position", grid.map_to_world(grid_position), grid.map_to_world(next_position), 
+			move_time, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
+	tween.start()
+	#wait for the tween to be over before continuing
+	yield(tween,"tween_completed")
+	
+	#we now allow _process to be called on the next frame since we are finished moving
+	#this will allow us to get our next position and move to it
+	set_process(true)
+	
+#Set the Enemy behavior when he is eaten by Pacman
+func get_eaten() :
+	current_behavior = behavior.DEAD
+
+#Set the Enemy bheavior when one of the has eaten Pacman
+func set_victorious() :
+	current_behavior = behavior.VICTORIOUS
+
+#retrun true if our current behavior is FOLLOW
+func is_following() : 
+	return current_behavior == behavior.FOLLOW
+
+#retrun true if our current behavior is FLEE
+func is_fleeing() : 
+	return current_behavior == behavior.FLEE
---
+++ Grid.gd
@@ -0,0 +1,149 @@
+extends TileMap
+
+#The map size in number of cells
+export var map_size = Vector2(22,10)
+
+#pointer to the player
+onready var player = $Player
+
+#pointer to the Inky ghost
+onready var Inky = $Inky
+
+#pointer to the Blinky ghost
+onready var Blinky = $Blinky
+
+#pointer to the Pinky ghost
+onready var Pinky = $Pinky
+
+#pointer to the Clyde ghost
+onready var Clyde = $Clyde
+
+#Array of the enemies (useful for group actions)
+onready var enemies = [Inky, Blinky, Pinky, Clyde]
+
+#setting up a pathfinding algorithm to manage the ghosts
+var navigation = AStar.new()
+
+#ID of walls on the gridmap
+var WALLS := [0,1]
+
+#list of Vector2 of walls cells (in grid coordidates)
+var wall_list : Array
+
+#list of Vector2 of the walkable cells for the pathfinding algorithm (in grid coordidates)
+var walkable_cells : Array
+
+#list of Vector2 of the bonuses position (in grid coordidates)
+var bonuses_position : Array
+
+#return a Vector2 of the player's position in grid coordinates
+func get_player_pos() -> Vector2:
+	#when you work with a grid you often need to go from real coordinates (the pixels in the window)
+	#to grid coordinates (the cell's coordinates inside the grid
+	#for this we have two functions : 
+	#	world_to_map -> converts coordinates in the window into grid coordinates
+	#	map_to_world -> converts grid cell coordinates into window's pixel coordinates
+	#to ease working with maps, unless stated otherwise every coordinates pair are in grid coordinates
+	return(world_to_map(player.position))
+	
+#builds the class variable walkable_cells (I.E. get dynamicly all the cells that are not walls)
+#and add them to the pathfinding class
+func get_walkable_cells() -> void:
+	walkable_cells = []
+	for y in range(map_size.y):
+		for x in range(map_size.x):
+			var cell = Vector2(x,y)
+			if cell in wall_list : continue
+			var cell_id = cell.x + map_size.x * cell.y
+			navigation.add_point(cell_id, Vector3(cell.x,cell.y,0.0))
+			walkable_cells.append(cell)
+
+#return true if the cell is out of the grid's bounds
+func out_of_bounds(cell : Vector2) -> bool:
+	return cell.x < 0 or cell.y < 0 or cell.x >= map_size.x or cell.y >= map_size.y
+
+#builds automaticaly the graph of the walkable cells for the pathfinding algorithm
+#by connecting walkable cells that are next to each other
+func astar_connect_walkable_cells() -> void :
+	for cell in walkable_cells:
+		var cell_id = cell.x + map_size.x * cell.y
+		var ortho_neighbors = PoolVector2Array([
+			cell + Vector2(1,0),
+			cell + Vector2(-1,0),
+			cell + Vector2(0,1),
+			cell + Vector2(0,-1),
+		])
+		for neighbor in ortho_neighbors:
+			var neighbor_id = neighbor.x + map_size.x * neighbor.y
+			if out_of_bounds(neighbor) : continue
+			if not navigation.has_point(neighbor_id) : continue
+			navigation.connect_points(cell_id, neighbor_id, false)
+
+#return true if the cell from+direction is walkable
+func can_move(from : Vector2, direction : Vector2) -> bool:
+	var npos = from + direction
+	if npos in wall_list :
+		return false
+	return true
+
+#return the grid coordinates of the enemy
+func get_enemy_pos(id : String) -> Vector2:
+	match id:
+		"Inky":
+			return world_to_map(Inky.position)
+		"Blinky":
+			return world_to_map(Blinky.position)
+		"Pinky":
+			return world_to_map(Pinky.position)
+		"Clyde":
+			return world_to_map(Clyde.position)
+	return Vector2()
+
+#Returns the next cell to go for a path that goes from cell_from to cell_to
+func astar_get_next_cell(cell_from : Vector2, cell_to : Vector2) -> Vector2:
+	var cell_from_id = cell_from.x + map_size.x * cell_from.y
+	var cell_to_id = cell_to.x + map_size.x * cell_to.y
+	var path = navigation.get_point_path(cell_from_id, cell_to_id)
+	if path.size() > 1 :
+		return Vector2(path[1].x,path[1].y)
+	return Vector2()
+
+
+#Called once when the Game scene is loaded (like a constructor in OOP)
+func _ready():
+	wall_list = get_used_cells_by_id(0) + get_used_cells_by_id(1)
+	get_walkable_cells() 
+	astar_connect_walkable_cells()
+	player.position = map_to_world(Vector2(1,1))
+	for enemy in enemies:
+		enemy.position = map_to_world(enemy.start_position)
+
+
+#called every frame, thats where the logic of the game is implemented
+#I.E. check if the player is eaten by ghosts and so on 
+func _process(delta):
+	if player.grid_position == Vector2():
+		return
+	print('player : ',player.grid_position)
+	for enemy in enemies :
+		if player.grid_position == enemy.grid_position :
+			print('enemy : ',enemy.grid_position)
+			if enemy.is_following():
+				player.get_eaten()
+				Inky.set_victorious()
+				Blinky.set_victorious()
+				Pinky.set_victorious()
+				Clyde.set_victorious()
+				print("toto")
+			elif enemy.is_fleeing():
+				enemy.get_eaten()
+				player.eat_enemy()
+	#TODO : Create a Bonus scene and manage the bonus logic 
+	#	in the grid and in the player
+	#if player.grid_position in bonuses_position:
+	#	var bonus = get_bonus_at(player.grid_position)
+	#	player.eat_bonus(bonus.get_type())
+		
+			
+			
+			
